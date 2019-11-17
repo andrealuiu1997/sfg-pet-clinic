f17bfc2a646ccac241603b0790ab1df44e70a6c8
andrea luiu <andrea.luiu40@gmail.com>
2019-11-17 16:09:56 +0100

    Adding Index Page and controller, closes #8.

---
+++ pet-clinic-web/src/main/java/andrea/springframework/sfgperclinic/controller/IndexController.java
@@ -0,0 +1,13 @@
+package andrea.springframework.sfgperclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String index(){
+        return "index";
+    }
+}
