c67cc60b1d45c80a7018f136216bb2c2ee71a006
andrea luiu <andrea.luiu40@gmail.com>
2019-11-17 15:14:39 +0100

    creating the map services. closes #6

---
+++ pet-clinic-data/src/main/java/andrea/springframework/sfgperclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package andrea.springframework.sfgperclinic.services.map;
+
+import andrea.springframework.sfgperclinic.Model.Pet;
+import andrea.springframework.sfgperclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet,Long> implements CrudService<Pet,Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet findByID(Long id) {
+        return super.findById(id);
+    }
+
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/andrea/springframework/sfgperclinic/services/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package andrea.springframework.sfgperclinic.services.map;
+
+import andrea.springframework.sfgperclinic.Model.Vet;
+import andrea.springframework.sfgperclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet findByID(Long id) {
+        return super.findById(id);
+    }
+
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteById(id);
+    }
+}
