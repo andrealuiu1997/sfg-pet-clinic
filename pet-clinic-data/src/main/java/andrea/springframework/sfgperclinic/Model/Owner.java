17b2aadcab19d808bfb265b8e07656bca45a3959
andrea luiu <andrea.luiu40@gmail.com>
2019-12-05 11:42:02 +0100

    added visists, updated owner and reletionship. closes #27

--- pet-clinic-data/src/main/java/andrea/springframework/sfgperclinic/Model/Owner.java
+++ pet-clinic-data/src/main/java/andrea/springframework/sfgperclinic/Model/Owner.java
@@ -1,4 +1,18 @@
 package andrea.springframework.sfgperclinic.Model;
 
+import java.util.Set;
+
 public class Owner extends Person {
+
+    Set<Pet> pets;
+
+
+    //getters and setters
+    public Set<Pet> getPets() {
+        return pets;
+    }
+
+    public void setPets(Set<Pet> pets) {
+        this.pets = pets;
+    }
 }
