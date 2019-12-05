f81ac6bdbd446d2c4543a30c648c7cbc57f637ce
andrea luiu <andrea.luiu40@gmail.com>
2019-12-05 11:55:50 +0100

    oener updated. Closes #29

--- pet-clinic-data/src/main/java/andrea/springframework/sfgperclinic/Model/Owner.java
+++ pet-clinic-data/src/main/java/andrea/springframework/sfgperclinic/Model/Owner.java
@@ -4,10 +4,38 @@ import java.util.Set;
 
 public class Owner extends Person {
 
-    Set<Pet> pets;
+    private String city;
+    private String address;
+    private String telephone;
+    private Set<Pet> pets;
 
 
     //getters and setters
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+
     public Set<Pet> getPets() {
         return pets;
     }
